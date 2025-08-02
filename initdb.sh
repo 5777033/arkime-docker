953f568e7faa408e0f778c8ee8377e605f034e15
Jensen <77828849+5777033@users.noreply.github.com>
2025-08-02 18:18:14 +0100

    Update initdb.sh

--- initdb.sh
+++ initdb.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+# 初始化 Arkime 数据库（如果未初始化）
 /opt/arkime/db/db.pl --insecure http://admin:${OPENSEARCH_INITIAL_ADMIN_PASSWORD}@elasticsearch:9200 init --ifneeded
-/opt/arkime/bin/arkime_add_user.sh ${VIEWER_ADMIN_USERNAME} "Administrator" ${VIEWER_ADMIN_PASSWORD} --admin
 
+# 创建管理员账户
+/opt/arkime/bin/arkime_add_user.sh ${VIEWER_ADMIN_USERNAME} "Administrator" ${VIEWER_ADMIN_PASSWORD} --admin         
+
+# 创建你的自定义用户
+/opt/arkime/bin/arkime_add_user.sh test "Test User" test12345 --admin
 
 echo 'DB Info'
 echo '-------'
